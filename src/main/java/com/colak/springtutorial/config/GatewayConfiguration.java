44e5cb3bbbf9ca29f94c8c2c9a82edc3f47f7824
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-01 14:44:54 +0300

    change urls

--- src/main/java/com/colak/springtutorial/config/GatewayConfiguration.java
+++ src/main/java/com/colak/springtutorial/config/GatewayConfiguration.java
@@ -8,12 +8,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfiguration {
 
+    // http://localhost:8080/orders/1
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("orders", r -> r.path("/orders/*") // Match incoming requests to /orders/*
                         .filters(f -> f
-                                .rewritePath("/orders/(?<segment>.*)", "/forders/${segment}") // Rewrite the path
+                                .rewritePath("/orders/(?<segment>.*)", "/api/orders/${segment}") // Rewrite the path
                         )
                         .uri("http://localhost:8080")) // Forward to the ignored endpoint
                 .build();
--- src/main/java/com/colak/springtutorial/controller/OrderController.java
+++ src/main/java/com/colak/springtutorial/controller/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/forders")
+@RequestMapping("/api/orders")
 public class OrderController {
 
     // http://localhost:8080/orders/1
