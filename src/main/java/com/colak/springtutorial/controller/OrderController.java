4d691fdf01a61530a0bb64e3eb890f79b6c7e7c0
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-02 11:59:57 +0300

    move configuration to yaml file

--- src/main/java/com/colak/springtutorial/controller/OrderController.java
+++ src/main/java/com/colak/springtutorial/controller/OrderController.java
@@ -17,6 +17,8 @@ import java.util.Map;
 @Slf4j
 public class OrderController {
 
+    private int counter;
+
     // http://localhost:8080/orders/1
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderModel> getOrderById(@PathVariable String orderId,
@@ -27,8 +29,20 @@ public class OrderController {
         log.info("Received params: {}", allParams);
         log.info("Received headers: {}", allHeaders);
 
+        int oldValue = counter++;
+
+        // Throw exceptions to test Retry filter
+        if (isEven(oldValue)) {
+            throw new RuntimeException("Even number " + oldValue);
+        } else {
+            log.info("Odd number {}", oldValue);
+        }
         OrderModel orderModel = new OrderModel();
         orderModel.setOrderId(orderId);
         return ResponseEntity.ok(orderModel);
+    }
+
+    private boolean isEven(int value) {
+        return value % 2 == 0;
     }
 }
